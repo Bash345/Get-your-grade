1a5637949bbb9531592e02e809fc58b65a1a31b9
Bash345 <66363148+Bash345@users.noreply.github.com>
2022-01-31 11:34:58 +0000

    Add files via upload

---
+++ Get your Grade.py
@@ -0,0 +1,16 @@
+grade = float(input("Please enter your grade mark: "))
+
+if grade > 80 and grade <=100:
+       print ("A")
+elif grade > 70 and grade <=80:
+       print ("B")
+elif grade > 60 and grade <=70:
+
+       print ("C")
+elif grade > 50 and grade <=60:
+
+       print ("D")
+elif grade <=50:
+    print ("F")
+else: 
+       print ("Marks are between 0 and 100")
